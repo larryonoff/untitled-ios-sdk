a2a903ebd0d273009062cdb6b05d8c1fc33da9b5
Ilya Laryionau <larryonoff@gmail.com>
2022-03-23 12:17:47 +0400

    Add Bundle.appsFlyerAPIKey and Bundle.appleID

--- Sources/FoundationExt/Bundle.swift
+++ Sources/FoundationExt/Bundle.swift
@@ -14,4 +14,12 @@ extension Bundle {
   public var amplitudeAPIKey: String? {
     self["XAmplitudeAPIKey"] as? String
   }
+
+  public var appsFlyerAPIKey: String? {
+    self["XAppsFlyerAPIKey"] as? String
+  }
+
+  public var appleID: String? {
+    self["X_APPLE_ID"] as? String
+  }
 }
