beee1e7f8eb3735f44f40991eab662730b83a787
Ilya Laryionau <larryonoff@gmail.com>
2022-08-14 13:48:14 +0300

    Fix logger extensions

--- Sources/LoggerExt/Logger.swift
+++ Sources/LoggerExt/Logger.swift
@@ -1,8 +1,44 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ilya Laryionau on 14.08.22.
-//
+import CustomDump
+import os.log
 
-import Foundation
+extension Logger {
+  public func info(
+    _ message: String,
+    dump args: [String: Any]
+  ) {
+    var argsDump = ""
+    _ = customDump(args, to: &argsDump)
+
+    self.info("\(message)\n\(argsDump)")
+  }
+
+  public func debug(
+    _ message: String,
+    dump args: [String: Any]
+  ) {
+    var argsDump = ""
+    _ = customDump(args, to: &argsDump)
+
+    self.debug("\(message)\n\(argsDump)")
+  }
+
+  public func error(
+    _ message: String,
+    dump args: [String: Any]
+  ) {
+    var argsDump = ""
+    _ = customDump(args, to: &argsDump)
+
+    self.error("\(message)\n\(argsDump)")
+  }
+
+  public func fault(
+    _ message: String,
+    dump args: [String: Any]
+  ) {
+    var argsDump = ""
+    _ = customDump(args, to: &argsDump)
+
+    self.fault("\(message)\n\(argsDump)")
+  }
+}
