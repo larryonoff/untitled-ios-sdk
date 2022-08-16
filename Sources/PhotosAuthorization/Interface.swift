f8ef278596673f13ef566d080b67219f155b1bff
Ilya Laryionau <larryonoff@gmail.com>
2022-08-16 22:52:27 +0300

    Remove PhotosAuthorizationClient initilizer

--- Sources/PhotosAuthorization/Interface.swift
+++ Sources/PhotosAuthorization/Interface.swift
@@ -2,16 +2,6 @@ public struct PhotosAuthorizationClient {
   public var authorizationStatus: @Sendable (AccessLevel) -> AuthorizationStatus
   public var authorizationStatusUpdates: @Sendable (AccessLevel) -> AsyncStream<AuthorizationStatus>
   public var requestAuthorization: @Sendable (AccessLevel) async -> AuthorizationStatus
-
-  init(
-    authorizationStatus: @escaping @Sendable (AccessLevel) -> AuthorizationStatus,
-    authorizationStatusUpdates: @escaping @Sendable (AccessLevel) -> AsyncStream<AuthorizationStatus>,
-    requestAuthorization: @escaping @Sendable (AccessLevel) async -> AuthorizationStatus
-  ) {
-    self.authorizationStatus = authorizationStatus
-    self.authorizationStatusUpdates = authorizationStatusUpdates
-    self.requestAuthorization = requestAuthorization
-  }
 }
 
 extension PhotosAuthorizationClient {
