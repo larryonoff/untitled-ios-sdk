549ea15dc7113ab83f1541d7ec544bcf3fc6e4f1
Ilya Laryionau <larryonoff@gmail.com>
2022-11-01 16:08:50 +0300

    Extend BidirectionalCollection with next(after:), next(before:)

---
+++ Sources/FoundationSupport/BidirectionalCollection.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+extension BidirectionalCollection {
+  @inlinable
+  public func next(
+    after predicate: (Element) throws -> Bool,
+    loop: Bool = false
+  ) rethrows -> Element? {
+    guard let idx = try firstIndex(where: predicate) else {
+      return nil
+    }
+
+    let nextIdx = index(after: idx)
+
+    if nextIdx < endIndex {
+      return self[nextIdx]
+    }
+
+    return loop ? first : nil
+  }
+
+  @inlinable
+  public func next(
+    before predicate: ((Element) throws -> Bool),
+    loop: Bool = false
+  ) rethrows -> Element? {
+    guard let idx = try firstIndex(where: predicate) else {
+      return nil
+    }
+
+    let nextIdx = index(before: idx)
+
+    if nextIdx < startIndex {
+      return loop ? last : nil
+    } else {
+      return self[nextIdx]
+    }
+  }
+}
+
+extension BidirectionalCollection where Element: Equatable {
+  public func next(
+    after element: (Element),
+    loop: Bool = false
+  ) -> Element? {
+    next(after: { $0 == element })
+  }
+
+  public func next(
+    before element: (Element),
+    loop: Bool = false
+  ) -> Element? {
+    next(before: { $0 == element })
+  }
+}
