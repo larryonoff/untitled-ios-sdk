ae22852db3522d58675abe997f56bdb4c053da9d
Ilya Laryionau <larryonoff@gmail.com>
2022-11-12 11:09:05 +0300

    Add synchronize to View

---
+++ Sources/SwiftUIExt/View+Synchronize.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension View {
+  public func synchronize<Value>(
+    _ first: Binding<Value>,
+    _ second: FocusState<Value>.Binding
+  ) -> some View {
+    self
+      .onChange(of: first.wrappedValue) { second.wrappedValue = $0 }
+      .onChange(of: second.wrappedValue) { first.wrappedValue = $0 }
+  }
+}
