772bb52ce02fda7d78c6bc1675c6c6ca7eb62f6e
Ilya Laryionau <larryonoff@gmail.com>
2022-12-05 13:15:22 +0100

    Rename Purchases.isTrialAvailable -> isEligibleForIntroductoryOffer

--- Sources/PurchasesClient/Model/Purchases.swift
+++ Sources/PurchasesClient/Model/Purchases.swift
@@ -2,8 +2,8 @@ import Adapty
 import Foundation
 
 public struct Purchases {
+  public var isEligibleForIntroductoryOffer: Bool = true
   public var isPremium: Bool = false
-  public var isTrialAvailable: Bool = true
 }
 
 extension Purchases: Codable {}
@@ -20,8 +20,8 @@ extension Purchases {
       let purchaserInfo = purchaserInfo,
       let accessLevel = purchaserInfo.accessLevels["premium"]
     else {
+      self.isEligibleForIntroductoryOffer = true
       self.isPremium = false
-      self.isTrialAvailable = true
       return
     }
 
@@ -29,8 +29,8 @@ extension Purchases {
       .values
       .map(\.vendorProductId)
 
+    self.isEligibleForIntroductoryOffer = subscriptionsProductIDs.isEmpty
     self.isPremium = accessLevel.isActive
-    self.isTrialAvailable = subscriptionsProductIDs.isEmpty
   }
 }
 
@@ -44,12 +44,12 @@ extension Purchases {
   /// when internet connection is not available,
   /// so it's important to load previous values
   static func load() -> Purchases {
-    let purchaserInfo = UserDefaults.standard
+    let profile = UserDefaults.standard
       .object(forKey: .purchaserInfoKey)
       .flatMap { $0 as? Data }
       .flatMap { try? JSONDecoder().decode(AdaptyProfile.self, from: $0) }
 
-    return Purchases(purchaserInfo)
+    return Purchases(profile)
   }
 }
 
