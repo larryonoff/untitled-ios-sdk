633775bfc59e3f5d4517a8b4dbec911ba73a0eca
Ilya Laryionau <larryonoff@gmail.com>
2022-12-28 17:05:29 +0100

    Add PHAssetCollection assetCount, smartAlbumUserLibrary

--- Sources/PhotosExt/PHAssetCollection.swift
+++ Sources/PhotosExt/PHAssetCollection.swift
@@ -1,6 +1,33 @@
 import Photos
 import Tagged
 
+extension PHAssetCollection {
+  public var assetCount: Int {
+    let fetchOptions = PHFetchOptions()
+    fetchOptions.wantsIncrementalChangeDetails = false
+
+    return PHAsset.fetchAssets(
+      in: self,
+      options: fetchOptions
+    )
+    .count
+  }
+
+  public static var smartAlbumUserLibrary: PHAssetCollection? {
+    let options = PHFetchOptions()
+    options.fetchLimit = 1
+    options.includeAllBurstAssets = false
+
+    return PHAssetCollection
+      .fetchAssetCollections(
+        with: .smartAlbum,
+        subtype: .smartAlbumUserLibrary,
+        options: options
+      )
+      .firstObject
+  }
+}
+
 extension PHAssetCollection: Identifiable {
   public typealias ID = Tagged<PHAssetCollection, String>
 
