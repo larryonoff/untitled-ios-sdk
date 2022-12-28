226fbf542e12bbfb1869d5af486baaf20450e8ac
Ilya Laryionau <larryonoff@gmail.com>
2022-12-28 17:03:35 +0100

    Add PHPhotoLibrary.changes

---
+++ Sources/PhotosExt/PHPhotoLibrary.swift
@@ -0,0 +1,35 @@
+import ComposableArchitecture
+import Photos
+
+extension PHPhotoLibrary {
+  public var changes: AsyncStream<PHChange> {
+    AsyncStream<PHChange> { continuation in
+      let sink = PhotoLibraryChangeObserverSink()
+
+      let task = Task {
+        self.register(sink)
+
+        for await element in sink.pipe.stream {
+          continuation.yield(element)
+        }
+        continuation.finish()
+      }
+
+      continuation.onTermination = { _ in
+        self.unregisterChangeObserver(sink)
+        task.cancel()
+      }
+    }
+  }
+}
+
+
+private final class PhotoLibraryChangeObserverSink: NSObject {
+  fileprivate let pipe = AsyncStream.streamWithContinuation(PHChange.self)
+}
+
+extension PhotoLibraryChangeObserverSink: PHPhotoLibraryChangeObserver {
+  func photoLibraryDidChange(_ changeInstance: PHChange) {
+    pipe.continuation.yield(changeInstance)
+  }
+}
