ff9ef71a40d402dc0baf9e59fa591390e5d19e70
Ilya Laryionau <larryonoff@gmail.com>
2023-01-24 11:16:44 +0100

    Extend UIImage(symbol:) with configuration parameter

--- Sources/SFSymbol/UIImage.swift
+++ Sources/SFSymbol/UIImage.swift
@@ -5,9 +5,17 @@ import UIKit
 extension UIImage {
   /// Create an image using a system symbol image of the given type.
   ///
-  /// - Parameter symbol: The `SFSymbol` describing this image.
-  public convenience init?(symbol: SFSymbol) {
-    self.init(systemName: symbol.rawValue)
+  /// - Parameters:
+  ///   - symbol: The `SFSymbol` describing this image.
+  ///   - configuration: The symbol configuration the system applies to the image.
+  public convenience init?(
+    symbol: SFSymbol,
+    withConfiguration configuration: Configuration? = nil
+  ) {
+    self.init(
+      systemName: symbol.rawValue,
+      withConfiguration: configuration
+    )
   }
 }
 
