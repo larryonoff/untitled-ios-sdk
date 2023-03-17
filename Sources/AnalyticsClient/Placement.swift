276db1694b2bd9a1ec01ba9ab810ebb754233b81
Ilya Laryionau <larryonoff@gmail.com>
2023-03-17 13:11:24 +0100

    Add newSession placement

--- Sources/AnalyticsClient/Placement.swift
+++ Sources/AnalyticsClient/Placement.swift
@@ -3,3 +3,7 @@ import Tagged
 public enum PlacementTag: Sendable {}
 
 public typealias Placement = Tagged<PlacementTag, String>
+
+extension Placement {
+  public static let newSession: Self = "start_session"
+}
