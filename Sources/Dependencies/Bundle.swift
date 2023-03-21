51c9d9fe40120abfbd43ccce974fb3bf4f345311
Ilya Laryionau <larryonoff@gmail.com>
2023-03-21 16:16:20 +0100

    Replace Bundle with BundleInfo
    Bundle doesn't conform `Sendable`

--- Sources/Dependencies/Bundle.swift
+++ Sources/Dependencies/Bundle.swift
@@ -2,12 +2,30 @@ import Foundation
 import Dependencies
 
 extension DependencyValues {
-  public var bundle: Bundle {
+  public var bundle: BundleInfo {
     get { self[BundleKey.self] }
     set { self[BundleKey.self] = newValue }
   }
 
   private enum BundleKey: DependencyKey {
-    static let liveValue = Bundle.main
+    static let liveValue = BundleInfo(Bundle.main)
   }
 }
+
+public struct BundleInfo {
+  public var bundleName: String?
+  public var bundleShortVersion: String?
+  public var bundleVersion: String?
+
+  init(_ bundle: Bundle) {
+    self.bundleName = bundle.infoDictionary?["CFBundleName"] as? String
+    self.bundleShortVersion = bundle.infoDictionary?["CFBundleShortVersionString"] as? String
+    self.bundleVersion = bundle.infoDictionary?["CFBundleVersion"] as? String
+  }
+}
+
+extension BundleInfo: Equatable {}
+
+extension BundleInfo: Hashable {}
+
+extension BundleInfo: Sendable {}
