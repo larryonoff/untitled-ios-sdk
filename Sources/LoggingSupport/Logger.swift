9f5fe7ed7e1ac910e118eb706379013686d50f95
Ilya Laryionau <larryonoff@gmail.com>
2023-05-11 11:43:03 +0200

    Update LoggingExt with auto-closures and new log levels

--- Sources/LoggingSupport/Logger.swift
+++ Sources/LoggingSupport/Logger.swift
@@ -3,42 +3,50 @@ import os.log
 
 extension Logger {
   public func info(
-    _ message: String,
-    dump args: [String: Any]
+    _ message: @autoclosure @escaping () -> String,
+    dump args: @autoclosure @escaping () -> [String: Any]
   ) {
-    var argsDump = ""
-    _ = customDump(args, to: &argsDump)
-
-    self.info("\(message)\n\(argsDump)")
+    let argsDump = String(customDumping: args())
+    self.info("\(message())\n\(argsDump)")
   }
 
   public func debug(
-    _ message: String,
-    dump args: [String: Any]
+    _ message: @autoclosure @escaping () -> String,
+    dump args: @autoclosure @escaping () -> [String: Any]
   ) {
-    var argsDump = ""
-    _ = customDump(args, to: &argsDump)
+    let argsDump = String(customDumping: args())
+    self.debug("\(message())\n\(argsDump)")
+  }
 
-    self.debug("\(message)\n\(argsDump)")
+  public func warning(
+    _ message: @autoclosure @escaping () -> String,
+    dump args: @autoclosure @escaping () -> [String: Any]
+  ) {
+    let argsDump = String(customDumping: args())
+    self.warning("\(message())\n\(argsDump)")
   }
 
   public func error(
-    _ message: String,
-    dump args: [String: Any]
+    _ message: @autoclosure @escaping () -> String,
+    dump args: @autoclosure @escaping () -> [String: Any]
   ) {
-    var argsDump = ""
-    _ = customDump(args, to: &argsDump)
+    let argsDump = String(customDumping: args())
+    self.error("\(message())\n\(argsDump)")
+  }
 
-    self.error("\(message)\n\(argsDump)")
+  public func critical(
+    _ message: @autoclosure @escaping () -> String,
+    dump args: @autoclosure @escaping () -> [String: Any]
+  ) {
+    let argsDump = String(customDumping: args())
+    self.critical("\(message())\n\(argsDump)")
   }
 
   public func fault(
-    _ message: String,
-    dump args: [String: Any]
+    _ message: @autoclosure @escaping () -> String,
+    dump args: @autoclosure @escaping () -> [String: Any]
   ) {
-    var argsDump = ""
-    _ = customDump(args, to: &argsDump)
-
-    self.fault("\(message)\n\(argsDump)")
+    let argsDump = String(customDumping: args())
+    self.fault("\(message())\n\(argsDump)")
   }
 }
