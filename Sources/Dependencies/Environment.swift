b04e9dd2e42c206efba9d43fdfffe4dbb6eb61f1
Ilya Laryionau <larryonoff@gmail.com>
2023-05-30 10:47:23 +0200

    Add environment dependency value

--- Sources/Dependencies/Environment.swift
+++ Sources/Dependencies/Environment.swift
@@ -1,5 +1,12 @@
 import Dependencies
 
+extension DependencyValues {
+  public var environment: Environment {
+    get { self[Environment.self] }
+    set { self[Environment.self] = newValue }
+  }
+}
+
 public enum Environment {
   case production
   case staging
@@ -10,3 +17,7 @@ extension Environment: Equatable {}
 extension Environment: Hashable {}
 
 extension Environment: Sendable {}
+
+extension Environment: TestDependencyKey {
+  public static let testValue = Environment.staging
+}
