f22402076bfb3ef09086c456dc002be692b97580
Ilya Laryionau <larryonoff@gmail.com>
2023-06-15 15:04:21 +0200

    Update navigation link extension
    - add composable navigationLinkDestination
    - rename pushToNavigationStack to navigationLinkDestination

---
+++ Sources/ComposableArchitectureExt/NavigationLink.swift
@@ -0,0 +1,20 @@
+@_spi(Presentation) import ComposableArchitecture
+import SwiftUI
+import SwiftUIExt
+
+extension View {
+  @available(iOS, introduced: 13, deprecated: 16)
+  @available(macOS, introduced: 10.15, deprecated: 13)
+  @available(tvOS, introduced: 13, deprecated: 16)
+  @available(watchOS, introduced: 6, deprecated: 9)
+  public func navigationLinkDestination<State, Action, Destination: View>(
+    store: Store<PresentationState<State>, PresentationAction<Action>>,
+    @ViewBuilder content: @escaping (Store<State, Action>) -> Destination
+  ) -> some View {
+    self.presentation(store: store) { `self`, $isPresented, destinationContent in
+      self.navigationLinkDestination(isPresented: $isPresented) {
+        destinationContent(content)
+      }
+    }
+  }
+}
--- Sources/SwiftUI/NavigationLink.swift
+++ Sources/SwiftUI/NavigationLink.swift
@@ -1,30 +1,15 @@
 import SwiftUI
 
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
-extension NavigationLink where Label == EmptyView {
-  public init(
-    isActive: Binding<Bool>,
-    @ViewBuilder destination: () -> Destination
-  ) {
-    self.init(
-      isActive: isActive,
-      destination: destination,
-      label: EmptyView.init
-    )
-  }
-}
-
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension View {
-  @ViewBuilder
-  public func pushToNavigationStack<Content: View>(
+  public func navigationLinkDestination<Content: View>(
     isPresented: Binding<Bool>,
     @ViewBuilder content: () -> Content
   ) -> some View {
     background(
       NavigationLink(
         isActive: isPresented,
-        destination: content
+        destination: content,
+        label: EmptyView.init
       )
     )
   }
