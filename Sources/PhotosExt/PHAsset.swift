f0dc35a02c9fc96449a5bc2d924c986da127f418
Ilya Laryionau <larryonoff@gmail.com>
2023-07-11 18:38:11 +0200

    Add PHAsset async extetensions

--- Sources/PhotosExt/PHAsset.swift
+++ Sources/PhotosExt/PHAsset.swift
@@ -8,3 +8,29 @@ extension PHAsset: Identifiable {
     .init(localIdentifier)
   }
 }
+
+extension PHAsset {
+  public func requestContentEditingInput(
+    with options: PHContentEditingInputRequestOptions?
+  ) async -> (PHContentEditingInput?, [AnyHashable: Any]) {
+    var requestID: PHContentEditingInputRequestID?
+
+    let _onCancel = {
+      guard let requestID else { return }
+      self.cancelContentEditingInputRequest(requestID)
+    }
+
+    return await withTaskCancellationHandler {
+      await withCheckedContinuation { continuation in
+        requestID = self.requestContentEditingInput(
+          with: options,
+          completionHandler: { contentEditingInput, info in
+            continuation.resume(returning: (contentEditingInput, info))
+          }
+        )
+      }
+    } onCancel: {
+      _onCancel()
+    }
+  }
+}
