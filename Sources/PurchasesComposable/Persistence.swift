e5e6821f4444180d6653fb1f63ebb25e80c633cd
Ilya Laryionau <larryonoff@gmail.com>
2024-05-04 17:27:29 +0200

    Add PurchasesPersistenceKey

--- Sources/PurchasesComposable/Persistence.swift
+++ Sources/PurchasesComposable/Persistence.swift
@@ -1,8 +1,39 @@
 import ComposableArchitecture
 import DuckPurchasesClient
 
-extension PersistenceReaderKey where Self == InMemoryKey<Purchases> {
+extension PersistenceReaderKey where Self == PurchasesPersistenceKey {
   public static var purchases: Self {
-    inMemory("purchases")
+    PurchasesPersistenceKey()
   }
+}
+
+public struct PurchasesPersistenceKey: PersistenceReaderKey, Hashable, Sendable {
+  @Dependency(\.purchases) var purchases
+
+  public init() {}
+
+  public func load(initialValue: Purchases?) -> Purchases? {
+    purchases.purchases()
+  }
+
+  public func subscribe(
+    initialValue: Purchases?,
+    didSet: @escaping (Purchases?) -> Void
+  ) -> Shared<Purchases>.Subscription {
+    let task = Task {
+      for await purchases in self.purchases.purchasesUpdates() {
+        didSet(purchases)
+      }
+    }
+
+    return Shared.Subscription {
+      task.cancel()
+    }
+  }
+
+  public static func == (lhs: Self, rhs: Self) -> Bool {
+    true
+  }
+
+  public func hash(into hasher: inout Hasher) {}
 }
