36f0288d51893d8174d75c7c80383336b401fcf2
Ilya Laryionau <larryonoff@gmail.com>
2024-08-13 18:54:22 +0200

    Fix RequestReview dependency

--- Sources/Dependencies/RequestReview.swift
+++ Sources/Dependencies/RequestReview.swift
@@ -14,20 +14,14 @@ private enum RequestReviewKey: DependencyKey {
   static let liveValue = RequestReviewAction {
     let stream = AsyncStream<Bool> { continuation in
       let task = Task { @MainActor in
-        if #available(iOS 16.0, *) {
-          EnvironmentValues().requestReview()
-          continuation.yield(true)
-          continuation.finish()
+        if let activeScene = UIApplication.shared.activeScene {
+          SKStoreReviewController.requestReview(in: activeScene)
         } else {
-          if let activeScene = UIApplication.shared.activeScene {
-            SKStoreReviewController.requestReview(in: activeScene)
-          } else {
-            SKStoreReviewController.requestReview()
-          }
-
-          continuation.yield(false)
-          continuation.finish()
+          SKStoreReviewController.requestReview()
         }
+
+        continuation.yield(false)
+        continuation.finish()
       }
 
       continuation.onTermination = { @Sendable _ in
