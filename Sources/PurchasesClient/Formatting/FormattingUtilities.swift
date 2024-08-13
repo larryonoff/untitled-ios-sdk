00e470b0b0dd15cb82d5e9a5291b6123c2505c65
Ilya Laryionau <larryonoff@gmail.com>
2024-08-13 19:29:55 +0200

    Clean code

--- Sources/PurchasesClient/Formatting/FormattingUtilities.swift
+++ Sources/PurchasesClient/Formatting/FormattingUtilities.swift
@@ -3,12 +3,9 @@ import Foundation
 extension Int {
   var grammaticalNumber: Morphology.GrammaticalNumber {
     switch abs(self) {
-    case 0...1:
-      return .singular
-    case 1...4:
-      return .plural
-    default:
-      return .pluralTwo
+    case 0...1: .singular
+    case 1...4: .plural
+    default: .pluralTwo
     }
   }
 }
