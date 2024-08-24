78bf6cfbbda78985e3095e4d27a3e30b2849f6d1
Ilya Laryionau <larryonoff@gmail.com>
2024-08-24 13:22:57 +0200

    Add Placement.sceneDidBecomeActive

--- Sources/Core/Placement.swift
+++ Sources/Core/Placement.swift
@@ -4,6 +4,6 @@ public enum PlacementTag: Sendable {}
 public typealias Placement = Tagged<PlacementTag, String>
 
 extension Placement {
-  public static let didBecomeActive: Self = "did_become_active"
   public static let newSession: Self = "start_session"
+  public static let sceneDidBecomeActive: Self = "scene_become_active"
 }
