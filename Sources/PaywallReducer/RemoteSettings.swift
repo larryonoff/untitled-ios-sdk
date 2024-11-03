7472a564fe20470bc1b952550a5c11282cfffaf0
Ilya Laryionau <larryonoff@gmail.com>
2024-11-03 17:22:29 +0100

    Update RC for Purchases

--- Sources/PaywallReducer/RemoteSettings.swift
+++ Sources/PaywallReducer/RemoteSettings.swift
@@ -2,38 +2,24 @@ import ComposableArchitecture
 import DuckRemoteSettingsClient
 import DuckRemoteSettingsComposable
 
-extension RemoteSettingsClient {
-  public var isPaywallProductHiddenPricesEnabled: Bool {
-    boolForKey("paywall_product_hidden_price_enabled") ?? true
-  }
-
-  public var isPaywallOnboardingIntroOfferEnabled: Bool {
-    boolForKey("paywall_onboarding_intro_offer_enabled") ?? true
-  }
-
-  public var isPaywallOnboardingEnabled: Bool {
-    boolForKey("paywall_onboarding_enabled") ?? true
-  }
-}
-
 extension PersistenceReaderKey where Self == PersistenceKeyDefault<RemoteSettingKey<Bool>> {
   public static var isPaywallProductHiddenPricesEnabled: Self {
     PersistenceKeyDefault(
-      .remoteSetting("paywall_product_hidden_price_enabled"),
+      .remoteSetting(RemoteSettingsClient.isPaywallProductHiddenPricesEnabledKey),
       true
     )
   }
 
   public static var isPaywallOnboardingIntroOfferEnabled: Self {
     PersistenceKeyDefault(
-      .remoteSetting("paywall_onboarding_intro_offer_enabled"),
+      .remoteSetting(RemoteSettingsClient.isPaywallOnboardingIntroOfferEnabledKey),
       true
     )
   }
 
   public static var isPaywallOnboardingEnabled: Self {
     PersistenceKeyDefault(
-      .remoteSetting("paywall_onboarding_enabled"),
+      .remoteSetting(RemoteSettingsClient.isPaywallOnboardingEnabledKey),
       true
     )
   }
--- Sources/Purchases/RemoteSettings.swift
+++ Sources/Purchases/RemoteSettings.swift
@@ -1,6 +1,17 @@
 import DuckRemoteSettingsClient
 
 extension RemoteSettingsClient {
+  public var isPaywallProductHiddenPricesEnabled: Bool {
+    boolForKey(Self.isPaywallProductHiddenPricesEnabledKey) ?? true
+  }
+
+  public var isPaywallOnboardingIntroOfferEnabled: Bool {
+    boolForKey(Self.isPaywallOnboardingIntroOfferEnabledKey) ?? true
+  }
+
+  public var isPaywallOnboardingEnabled: Bool {
+    boolForKey(Self.isPaywallOnboardingEnabledKey) ?? true
+  }
 
   public var paywallPromoOffer: Paywall.PromoOffer? {
     let string = stringForKey(Self.paywallPromoOffer)
@@ -11,6 +22,18 @@ extension RemoteSettingsClient {
 }
 
 extension RemoteSettingsClient {
+  public static var isPaywallProductHiddenPricesEnabledKey: String {
+    "paywall_product_hidden_price_enabled"
+  }
+
+  public static var isPaywallOnboardingIntroOfferEnabledKey: String {
+    "paywall_onboarding_intro_offer_enabled"
+  }
+
+  public static var isPaywallOnboardingEnabledKey: String {
+    "paywall_onboarding_enabled"
+  }
+
   public static var paywallPromoOffer: String {
     "paywall_promo_offer"
   }
