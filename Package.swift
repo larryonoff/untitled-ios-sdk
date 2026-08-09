c9d9f5192a2e3c7b7620fb5599016e8794bc4be5
Ilya Laryionau <larryonoff@gmail.com>
2026-08-09 12:52:22 +0200

    fix: support Facebook client on Mac Catalyst

--- Package.swift
+++ Package.swift
@@ -921,11 +921,11 @@ extension Target.Dependency {
     )
 
     enum Facebook {
-      // iOS-only: XCFrameworks ship no macOS slice.
+      // The XCFramework ships iOS and Mac Catalyst slices, but no native macOS slice.
       static let core = product(
         name: "FacebookCore",
         package: "facebook-ios-sdk",
-        condition: .when(platforms: [.iOS])
+        condition: .when(platforms: [.iOS, .macCatalyst])
       )
     }
 
